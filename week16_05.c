60520632625119f53d79de1c6298aa8ea7b7fb16
juyong0129 <developerjuyong0129@gmail.com>
2025-06-25 20:52:03 +0900

    week16

---
+++ week16_05.c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+	int a[10] = { 0 };
+	int max = 0;
+	srand(time(NULL));
+
+	for (int i = 0; i < 100; i++) {
+		a[rand() % 10]++;
+	}
+	for (int i = 0; i < 10; i++) {
+		printf("%d : %d\n", i, a[i]);
+	}
+	for (int i = 0; i < 10; i++) {
+		if (a[i] > a[max]) {
+			max = i;
+		}
+	}
+
+	printf("가장 많이 생성된 수 = %d", max);
+
+	return 0;
+}
---
+++ week16_06.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+	int a[10] = { 0 };
+	srand(time(NULL));
+	printf("==============================\n");
+	printf("주사위 면                빈도\n");
+	printf("==============================\n");
+
+	for (int i = 0; i < 60000; i++) {
+		a[rand() % 10]++;
+	}
+
+	for (int i = 0; i < 10; i++) {
+		printf("    %d                    %d\n", i, a[i]);
+	}
+
+	return 0;
+}
